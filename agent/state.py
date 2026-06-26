fb3521bff2527429ce280964dce3168bcec13819
Deric Walintukan <deric.walintukan@gmail.com>
2026-06-26 12:17:23 -0700

    add reducer to bot state

--- agent/state.py
+++ agent/state.py
@@ -1,10 +1,12 @@
-from typing import TypedDict
+from typing import Annotated, TypedDict
+from langchain_core.messages import BaseMessage
+from langgraph.graph import add_messages
 
 from models import Recipe
 
 
 class BotState(TypedDict):
     chat_id: int
-    messages: list[str]
+    messages: Annotated[list[BaseMessage], add_messages]
     user_message: str
     pending_recipe: Recipe | None
