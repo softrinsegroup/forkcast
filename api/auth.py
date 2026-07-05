3ecbd99ca6ab8bba8c9935544a49618f0910b7e0
Deric Walintukan <deric.walintukan@gmail.com>
2026-07-05 15:07:53 +0900

    implement google callback to store state in user

--- api/auth.py
+++ api/auth.py
@@ -3,12 +3,13 @@ import secrets
 from urllib.parse import urlencode
 from fastapi import APIRouter, Request
 from fastapi.routing import RedirectResponse
+import httpx
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
 
 
 @router.get("/google")
@@ -18,7 +19,7 @@ async def google_login(request: Request):
 
     params = {
         "client_id": GOOGLE_CLIENT_ID,
-        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": GOOGLE_REDIRECT_URI,
         "response_type": "code",
         "scope": "openid email profile",
         "state": state,
@@ -28,5 +29,32 @@ async def google_login(request: Request):
 
 
 @router.get("/google/callback")
-async def google_callback():
-    pass
+async def google_callback(request: Request, code: str, state: str):
+    assert state == request.session.pop("oauth_state"), "Invalid state"
+    async with httpx.AsyncClient() as client:
+        # Exchange code for tokens
+        token_res = await client.post(
+            "https://oauth2.googleapis.com/token",
+            data={
+                "code": code,
+                "client_id": GOOGLE_CLIENT_ID,
+                "client_secret": GOOGLE_CLIENT_SECRET,
+                "redirect_uri": GOOGLE_REDIRECT_URI,
+                "grant_type": "authorization_code",
+            },
+        )
+        tokens = token_res.json()
+
+        # Fetch profile
+        user_res = await client.get(
+            "https://www.googleapis.com/oauth2/v3/userinfo",
+            headers={"Authorization": f"Bearer {tokens['access_token']}"},
+        )
+        user = user_res.json()
+        print(f"Logged in as: {user}")
+        request.session["user"] = {
+            "id": user["sub"],
+            "email": user["email"],
+            "name": user["name"],
+        }
+        return RedirectResponse("/")
